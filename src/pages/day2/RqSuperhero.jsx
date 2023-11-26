faa99bff61e5a51862fc71461ce3f3869527778b
mentorvikash <mentorvikash@gmail.com>
2023-11-26 10:46:47 +0530

    Add manual refetching

--- src/pages/day2/RqSuperhero.jsx
+++ src/pages/day2/RqSuperhero.jsx
@@ -7,7 +7,7 @@ const fetchSuperHeros = async () => {
 };
 
 function RqSuperHero() {
-  const { data, isLoading, isError, error, isFetching } = useQuery(
+  const { data, isLoading, isError, error, isFetching, refetch } = useQuery(
     "fetch superheros data",
     fetchSuperHeros,
     {
@@ -15,16 +15,18 @@ function RqSuperHero() {
       // cacheTime: 100000,
       // refetchOnMount: true, // when mount refresh data, default ture
       // refetchOnWindowFocus: true, // when re-focus window it fetch fresh data from server
-
       // Polling -  for e-commerce site where data, changes every second, refetch data every 1 or 2 second by
-      refetchInterval: 2000, // false | time in seconds
-      refetchIntervalInBackground: true, // ture | false - allow fetch fetch fresh data even is user not on page
-
-      // Allow user to refetch data on user intration
+      // refetchInterval: 2000, // false | time in seconds
+      // refetchIntervalInBackground: true, // ture | false - allow fetch fetch fresh data even is user not on page
     }
   );
 
-  if (isLoading) return <h2>Loading........</h2>;
+  // Allow user to refetch data on user intration
+  const reftechDataOnUserIntration = () => {
+    refetch();
+  };
+
+  if (isLoading || isFetching) return <h2>Loading........</h2>;
 
   if (isError) return <h2 className="error">{error.message}</h2>;
 
@@ -33,6 +35,14 @@ function RqSuperHero() {
   return (
     <div>
       <div>
+        <button
+          className="blackButton"
+          type="button"
+          aria-label="button to refetch data"
+          onClick={reftechDataOnUserIntration}
+        >
+          Refetch Data
+        </button>
         {data?.length > 0 && (
           <div>
             {data.map((superhero, index) => (
