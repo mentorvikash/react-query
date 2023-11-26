b130d920149e3efdd00ee414041eb4a5f7f98a5c
mentorvikash <mentorvikash@gmail.com>
2023-11-26 09:04:27 +0530

    add css and nav bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import Day1 from "./pages/day1"
 import Routing from './routes';
 import { QueryClientProvider, QueryClient } from 'react-query'
 
 function App() {
-
   const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
-        <h1>Day1: Introduction</h1>
-        {/* <Day1 /> */}
+        <div className='navBarMain'>
+          <a href='/day2/superHero'>Day2 Superhero</a>
+          <a href='/day2/rq-SuperHero'>Day2 RQ Superhero</a>
+        </div>
         <Routing />
       </div>
     </QueryClientProvider>
